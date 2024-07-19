a30964824f7e206044f259b97fe0f9ba708f1d22
pinki-meena <148627550+pinkimeena13@users.noreply.github.com>
2024-07-19 11:23:02 +0530

    create card

--- ecommerce/src/app/app.component.ts
+++ ecommerce/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { FooterComponent } from "./footer/footer.component";
 import { CustomerComponent } from "./customer/customer.component";
 import { HeaderComponent } from './header/header.component';
 import { PosterComponent } from './poster/poster.component';
+import { CardsComponent } from './cards/cards.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, FooterComponent, CustomerComponent, HeaderComponent, PosterComponent]
+    imports: [RouterOutlet, FooterComponent, CustomerComponent, HeaderComponent, PosterComponent, CardsComponent]
 })
 export class AppComponent {
   title = 'ecommerce';
--- ecommerce/src/app/cards/cards.component.ts
+++ ecommerce/src/app/cards/cards.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './cards.component.css'
 })
 export class CardsComponent {
+  hoverIndex: number | null = null;
 
+  NewArrivals =[
+{id:'1' , img:"assets/Images/card1.png", title:'T-shrit with Tape Details', rating:"assets/Images/star10.png" , point:'4.5/5', price:'$120',isWishlisted: false  },
+{id:'2' , img:"assets/Images/card2.png", title:'skinny Fit Jeans',rating:"assets/Images/star10.png" , point:'3.5/5', price:'$240' , totalprice:'$260' , discount:'-20%',isWishlisted: false  },
+{id:'3' , img:"assets/Images/card3.png", title:'Checkered Shrit',rating:"assets/Images/star10.png" , point:'4.5/5', price:'$180',isWishlisted: false },
+{id:'4' , img:"assets/Images/card4.png",title:'Sleeve Striped T-shrit', rating:"assets/Images/star10.png" , point:'4.5/5', price:'$130'  , totalprice:'$160' , discount:'-30%' ,isWishlisted: false },
+
+  ]
+  TopSelling=[
+    {id:'1' , img:"assets/Images/card5.png", title:'Vertical Striped Shrit', rating:"assets/Images/star10.png" , point:'5.0/5', price:'$212' , totalprice:'$232' , discount:'-20%' ,isWishlisted: false   },
+{id:'2' , img:"assets/Images/card6.png", title:'Courage Graphic T-shrit',rating:"assets/Images/star10.png" , point:'4.5/5', price:'$145' ,isWishlisted: false  },
+{id:'3' , img:"assets/Images/card7.png", title:'Loose Fit Bermuda Shorts',rating:"assets/Images/star10.png" , point:'3.5/5', price:'$80' ,isWishlisted: false },
+{id:'4' , img:"assets/Images/card8.png",title:'Faded Skinny Jeans', rating:"assets/Images/star10.png" , point:'4.5/5', price:'$210' ,isWishlisted: false  },
+  ]
+  toggleWishlist(card: any) {
+    card.isWishlisted = !card.isWishlisted;
+  }
 }
