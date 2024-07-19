95441359a7131203219a71846ed7343ed081389e
Aditi-Agrawal07 <07aditiagrawal@gmail.com>
2024-07-19 11:23:37 +0530

    product-detail

--- ecommerce/src/app/app.component.ts
+++ ecommerce/src/app/app.component.ts
@@ -5,13 +5,14 @@ import { CustomerComponent } from "./customer/customer.component";
 import { HeaderComponent } from './header/header.component';
 import { PosterComponent } from './poster/poster.component';
 import { BrowseStyleComponent } from "./browse-style/browse-style.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, FooterComponent, CustomerComponent, HeaderComponent, PosterComponent, BrowseStyleComponent]
+    imports: [RouterOutlet, FooterComponent, CustomerComponent, HeaderComponent, PosterComponent, BrowseStyleComponent, ProductDetailComponent]
 })
 export class AppComponent {
   title = 'ecommerce';
