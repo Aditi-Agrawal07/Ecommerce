37710c2836cb8dd616e9f9281ff5ce0bf49abe98
Aditi-Agrawal07 <07aditiagrawal@gmail.com>
2024-07-24 10:43:57 +0530

    product Schema Created

--- server/model/product.js
+++ server/model/product.js
@@ -1,4 +1,16 @@
-const productSchema = new Mongoose.Schema({
-    product_n
-    
+import { Schema } from 'mongoose'
+const productSchema = new Schema({
+    product_image: { type: String },
+    product_name: { type: String },
+    description: { type: String },
+    category: { type: String },
+    size: {
+        type: String,
+        enum: ["XS, S,M,L,XL,XXL"]
+    },
+    color: { type: [String] },
+    price: { type: Number },
+    rating: { type: Number },
+    discount: { type: Number }
+
 })
