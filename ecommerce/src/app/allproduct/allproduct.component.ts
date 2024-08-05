9334f5c5476e3894eb41a225adf01237caa53878
Aditi-Agrawal07 <07aditiagrawal@gmail.com>
2024-08-05 11:06:37 +0530

    implement get all products

--- ecommerce/src/app/allproduct/allproduct.component.ts
+++ ecommerce/src/app/allproduct/allproduct.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductcardsComponent } from '../productcards/productcards.component';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-allproduct',
@@ -9,5 +10,20 @@ import { ProductcardsComponent } from '../productcards/productcards.component';
   styleUrl: './allproduct.component.css'
 })
 export class AllproductComponent {
+ constructor(private productService: ProductService){}
+ products!: []
+  ngOnInit() {
+    const data = this.productService.getAllProducts().subscribe((response)=>{
+      this.products = response
+      
+
+    });
+   
+
+    
+  }
+
+
+  
 
 }
--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -15,9 +15,10 @@ export const productController = {
 
     getProducts: async (request, response) => {
         try {
-            const product = await Product.find();
-            if(!product){
-                return response.status(404).json({message:"Product Not Found "})
+            const product = await productService.getProducts()
+            // const product = await Product.find();
+            if (!product) {
+                return response.status(404).json({ message: "Product Not Found " })
             }
             response.json(product)
             const requestBody = request.body
@@ -26,9 +27,17 @@ export const productController = {
         } catch (err) {
 
             console.error("Error while geting product", err);
-            response.status(500).json({message:'Server Error'});
+            response.status(500).json({ message: 'Server Error' });
         }
 
     },
-      
+
+    getProductById: async (request, response) => {
+        const id = request.param.id;
+
+        const product = await Product.findById(id)
+
+        console.log(product);
+    }
+
 }
